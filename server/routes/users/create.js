04ac9c8432aa9fca34d09dca99bc9ababbeecbe8
Khang Tran <ktra@email.unc.edu>
2022-10-07 13:23:17 -0400

    Refactored for pooling connection module

--- server/routes/users/create.js
+++ server/routes/users/create.js
@@ -1,56 +1,54 @@
 // Dependencies
-require('dotenv').config();
 const express = require('express');
 const fs = require('fs');
 const router = express.Router();
-const mysql = require('mysql2');
+const pool = require('../pool');
 
-// Sets a connection to PlanetScale using a connection string in the .env file
-const connection = mysql.createConnection(process.env.DATABASE_URL);
-// Connect to database and error checking
-connection.connect((err) => {
-  if (err) throw err;
-});
-console.log('Connected to PlanetScale at /users/create.js!');
-
-/**
- * GET request handler for returning users table
- * 
- * '/create' - route path will match requests to the /users/create route
- * req - Receives GET request
- * res - Send back HTTPS result
- */
-router.get('/', (req, res)=>{
-    fs.readFile('insert.html', 'utf8', (err, form) => {
-      // Error checking for bad file read
-      if (err) {
-        console.error(err);
-        return;
-      }
-      // Send form 
-      res.send(form);
-    });
-  });
-  
+pool.getConnection(function(err, connection) {
+  // Bad connection
+  if (err) throw err; 
   /**
-   * POST request handler for inserting new user in 'users' table
+   * GET request handler for returning users table
    * 
    * '/create' - route path will match requests to the /users/create route
    * req - Receives GET request
    * res - Send back HTTPS result
    */
-router.post('/', (req, res) => {
-    /**
-     * .execute(), prepared statement parameters are sent from the client as a serialized string and handled by the server
-     * The VALUES(?) is standard way to insert variables into a SQL statement using an array
-     */
-    connection.execute('INSERT INTO users (first_name, last_name, email, type) VALUES (?, ?, ?, ?);', [req.body.first_name, req.body.last_name, req.body.email, req.body.type], (err, rows, fields) => {
-      // Error checking for bad query
-      if (err) throw err;
-      
-      // Send HTTPS, redirect to root
-      res.redirect('/users');
+  router.get('/', (req, res)=>{
+      fs.readFile('insert.html', 'utf8', (err, form) => {
+        // Error checking for bad file read
+        if (err) {
+          console.error(err);
+          return;
+        }
+        // Send form 
+        res.send(form);
+      });
     });
-  });
-  
+    
+    /**
+     * POST request handler for inserting new user in 'users' table
+     * 
+     * '/create' - route path will match requests to the /users/create route
+     * req - Receives GET request
+     * res - Send back HTTPS result
+     */
+  router.post('/', (req, res) => {
+      /**
+       * .execute(), prepared statement parameters are sent from the client as a serialized string and handled by the server
+       * The VALUES(?) is standard way to insert variables into a SQL statement using an array
+       */
+      connection.execute('INSERT INTO users (first_name, last_name, email, type) VALUES (?, ?, ?, ?);', [req.body.first_name, req.body.last_name, req.body.email, req.body.type], (err, rows, fields) => {
+        // Error checking for bad query
+        if (err) throw err;
+        
+        // Send HTTPS, redirect to root
+        res.redirect('/users');
+
+        // Release connection
+        connection.release();
+      });
+    });
+
+});
 module.exports = router; 
--- server/routes/users/index.js
+++ server/routes/users/index.js
@@ -1,42 +1,40 @@
 // Dependencies
-require('dotenv').config();
 const express = require('express');
 const create = require('./create');
 const router = express.Router();
-const mysql = require('mysql2');
+const pool = require('../pool');
 
 // Routing 
 router.use('/create', create);
 
-// Sets a connection to PlanetScale using a connection string in the .env file
-const connection = mysql.createConnection(process.env.DATABASE_URL);
-// Connect to database and error checking
-connection.connect((err) => {
-  if (err) throw err;
-});
-console.log('Connected to PlanetScale at /users/index.js!');
+pool.getConnection(function(err, connection) {
+  // Bad connection
+  if (err) throw err; 
+  /**
+   * GET request handler for returning users table
+   * 
+   * '/' - route path will match requests to the root route (in this case it would be '/users')
+   * req - Receives GET request
+   * res - Send back HTTPS result
+   */
+  router.get('/', (req, res) => {
+      /** 
+       * .query(), parameter substitution is handled on the client, including objects
+       * 'SELECT * FROM users' is valid sql to select everything from the table 'users'
+       *  rows is an array containing each row as an object
+       *  fields is an array containing each field as an object
+      */
+      connection.query('SELECT * FROM users', (err, rows, fields) => {
+        // Error checking for bad query
+        if (err) throw err;
+        
+        // Send HTTPS
+        res.send(rows);
 
-/**
- * GET request handler for returning users table
- * 
- * '/' - route path will match requests to the root route (in this case it would be '/users')
- * req - Receives GET request
- * res - Send back HTTPS result
- */
-router.get('/', (req, res) => {
-    /** 
-     * .query(), parameter substitution is handled on the client, including objects
-     * 'SELECT * FROM users' is valid sql to select everything from the table 'users'
-     *  rows is an array containing each row as an object
-     *  fields is an array containing each field as an object
-    */
-    connection.query('SELECT * FROM users', (err, rows, fields) => {
-      // Error checking for bad query
-      if (err) throw err;
-      
-      // Send HTTPS
-      res.send(rows);
+        // Release connection
+        connection.release();
+      });
     });
-  });
-
+    
+});
 module.exports = router; 
