6e0694ae3f78c833b774e84699d3431123a8fc03
Gabriel Mercado Mendez <gabomm99@ad.unc.edu>
2022-10-11 20:05:20 -0400

    Updated frontend with routes to different pages

---
+++ client/src/pages/Admin.js
@@ -0,0 +1,12 @@
+//Landing Page for Boomerang Staff 
+import React from "react";
+
+function Admin(){
+    return(
+    <div>
+        <h1> Boomerang Staff, welcome to ATS</h1>
+    </div>
+    )
+}
+
+export default Admin;
---
+++ client/src/pages/Student.js
@@ -0,0 +1,13 @@
+ //Landing Page for Boomerang Student
+
+ import React from "react";
+
+ function Student(){
+    return(
+        <div>
+            <h1> Student, welcome to ATS</h1>
+        </div>
+    )
+ }
+
+ export default Student;
