add7c7f64b69cd57371fbab958b29761e09088f6
Khang Tran <ktra@email.unc.edu>
2022-10-21 18:32:11 -0400

    Doc

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,12 +1,20 @@
 // Renders all of the pages, both client and server side
-import { Component } from "react";
 // Globals.css can only be used in _app.js, any other stylesheet needs file name [Name].module.css to work
 import "../styles/globals.css";
 
+import Head from "next/head";
+
 // Arrow function, return is implied
 const App = ({ Component, pageProps }) => (
-  // Render all the components and their properties
-  <Component {...pageProps} />
+  // Can only return one element, must empty html tag
+  <>
+    <!--
+    <Head>
+      <title>My new cool app</title>
+    </Head>
+    // Render all the components and their properties
+    <Component {...pageProps} />
+  </>
 );
 
 export default App;
