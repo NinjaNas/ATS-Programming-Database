3b251819069dfa2603a8d76cd614353bfed60708
Khang Tran <ktra@email.unc.edu>
2022-10-21 23:23:36 -0400

    Create.js post

--- server/routes/users/create.js
+++ server/routes/users/create.js
@@ -35,6 +35,40 @@ pool.getConnection(function (err, connection) {
         connection.release();
       }
     );
+    //New session creator if student type given.
+    if (req.body.type == "student") {
+      //Pulls ID of current user
+      connection.query(
+        "SELECT id FROM users WHERE email=?",
+        [req.body.email],
+        (err, userId, fields) => {
+          if (err) throw err;
+
+          //Creates new session with ID
+          connection.execute(
+            "INSERT INTO session (user_id, intake_date, school_administrator, social_worker, school_counselor, student_pickup) VALUES (?, ?, ?, ?, ?, ?);",
+            [
+              userId[0].id,
+              req.body.intake_date,
+              req.body.school_admin,
+              req.body.social_worker,
+              req.body.school_counselor,
+              req.body.pickup,
+            ],
+            (err, items, fields) => {
+              // Error checking for bad query
+              if (err) throw err;
+
+              // Release connection
+              connection.release();
+
+              console.log("More success!");
+            }
+          );
+        }
+      );
+    }
   });
 });
+
 module.exports = router;
