bb0cdc33e292a4a51e1596587e49471369f67415
Khang Tran <ktra@email.unc.edu>
2022-10-21 22:35:23 -0400

    Removed client folder

--- client/README.md
+++
@@ -1,9 +0,0 @@
-# Client Pages
-
-Sign In: `/`
-
-Sign Up: `/sign-up`
-
-Admin: `/admin`
-
-Student: `/student`
--- client/pages/about.js
+++
@@ -1,8 +0,0 @@
-const About = () => (
-  <section>
-    <h1>About Next JS</h1>
-    <p>Test</p>
-  </section>
-);
-
-export default About;
