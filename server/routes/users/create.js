7652897f34feda2a5bcc5fc68128510292f2e75a
Khang Tran <ktra@email.unc.edu>
2022-10-23 03:38:41 -0400

    Readded user_id

--- server/routes/users/create.js
+++ server/routes/users/create.js
@@ -8,8 +8,7 @@ pool.getConnection(function (err, connection) {
   // Bad connection
   if (err) throw err;
 
-  // async function to make sure the session execute goes after
-  router.post("/", async (req, res) => {
+  router.post("/", (req, res) => {
     // Object destructuring
     const {
       first_name,
@@ -23,7 +22,7 @@ pool.getConnection(function (err, connection) {
       pickup,
     } = req.body;
 
-    await connection.execute(
+    connection.execute(
       "SELECT * FROM users WHERE email=?",
       [email],
       (err, rows, fields) => {
@@ -31,11 +30,11 @@ pool.getConnection(function (err, connection) {
 
         // falsey, if 0 then there is no user with the same email
         if (rows.length) {
+          // Error out if email exists
           res.status(400).send({ msg: "Email already exists!" });
         } else {
           // Use hash function from utils/bcrypt.js
           const password_hash = hash(req.body.password_hash);
-          console.log(password_hash);
           /**
            * .execute(), prepared statement parameters are sent from the client as a serialized string and handled by the server
            * The VALUES(?) is standard way to insert variables into a SQL statement using an array
@@ -54,36 +53,45 @@ pool.getConnection(function (err, connection) {
           );
         }
 
+        //New session creator if student type given.
+        if (type == "student") {
+          //Pulls ID of current user
+          connection.query(
+            "SELECT id FROM users WHERE email=?",
+            [email],
+            (err, user_id, fields) => {
+              if (err) throw err;
+
+              //Creates new session with ID
+              connection.execute(
+                "INSERT INTO session (user_id, intake_date, school_administrator, social_worker, school_counselor, student_pickup) VALUES (?, ?, ?, ?, ?, ?);",
+                [
+                  user_id[0].id,
+                  intake_date,
+                  school_admin,
+                  social_worker,
+                  school_counselor,
+                  pickup,
+                ],
+                (err, rows, fields) => {
+                  // Error checking for bad query
+                  if (err) throw err;
+
+                  console.log("More success!");
+
+                  // Release connection
+                  connection.release();
+                }
+              );
+            }
+          );
+        }
+
         // Release connection
         connection.release();
       }
     );
-
-    //New session creator if student type given.
-    if (type == "student") {
-      //Creates new session with ID
-      await connection.execute(
-        "INSERT INTO session (intake_date, school_administrator, social_worker, school_counselor, student_pickup) VALUES (?, ?, ?, ?, ?);",
-        [
-          // Figure out if this is needed, foreign keys?
-          // rows[0].id,
-          intake_date,
-          school_admin,
-          social_worker,
-          school_counselor,
-          pickup,
-        ],
-        (err, rows, fields) => {
-          // Error checking for bad query
-          if (err) throw err;
-
-          console.log("More success!");
-
-          // Release connection
-          connection.release();
-        }
-      );
-    }
+    // Successful HTTPS
     res.sendStatus(201);
   });
 });
