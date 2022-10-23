76143c30b8c43b47cc78327920a9efca53652149
Khang Tran <ktra@email.unc.edu>
2022-10-23 03:06:57 -0400

    utils

--- server/utils/pool.js
+++ server/utils/pool.js
@@ -4,4 +4,5 @@ const mysql = require("mysql2");
 // Sets a pool of 10 (default value) connections to PlanetScale using a connection string in the .env file
 const pool = mysql.createPool(process.env.DATABASE_URL);
 console.log("Connected to PlanetScale!");
+
 module.exports = pool;
