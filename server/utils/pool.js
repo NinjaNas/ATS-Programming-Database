a5f8d0828a190b6511e04baf53a446eaa19e7f75
Khang Tran <ktra@email.unc.edu>
2022-10-23 13:33:35 -0400

    Use mysql2/promise and move getConnection function

--- server/utils/pool.js
+++ server/utils/pool.js
@@ -1,8 +1,15 @@
 require("dotenv").config();
-const mysql = require("mysql2");
+const mysql = require("mysql2/promise");
 
 // Sets a pool of 10 (default value) connections to PlanetScale using a connection string in the .env file
 const pool = mysql.createPool(process.env.DATABASE_URL);
+
+// getConnection function
+const getConnection = () =>
+  pool.getConnection((err) => {
+    if (err) throw err;
+  });
+
 console.log("Connected to PlanetScale!");
 
-module.exports = pool;
+module.exports = { getConnection };
