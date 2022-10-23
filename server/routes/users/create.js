ff511b924e433abdae6412e63a398d24a992afde
Khang Tran <ktra@email.unc.edu>
2022-10-23 13:34:11 -0400

    async/await, added promises for no nesting

--- server/routes/users/create.js
+++ server/routes/users/create.js
@@ -2,98 +2,103 @@
 const express = require("express");
 const router = express.Router();
 const { hash } = require("../../utils/bcrypt");
-const pool = require("../../utils/pool");
+const { getConnection } = require("../../utils/pool");
 
-pool.getConnection(function (err, connection) {
-  // Bad connection
-  if (err) throw err;
+/**
+ * async function, using mysql2/promise wrapper
+ * https://www.npmjs.com/package/mysql2#using-promise-wrapper
+ */
+router.post("/", async (req, res) => {
+  // Await connection
+  let connection = await getConnection();
 
-  router.post("/", (req, res) => {
-    // Object destructuring
-    const {
-      first_name,
-      last_name,
-      email,
-      type,
-      intake_date,
-      school_admin,
-      social_worker,
-      school_counselor,
-      pickup,
-    } = req.body;
+  // Object destructuring
+  const {
+    first_name,
+    last_name,
+    email,
+    type,
+    intake_date,
+    school_admin,
+    social_worker,
+    school_counselor,
+    pickup,
+  } = req.body;
 
-    connection.execute(
-      "SELECT * FROM users WHERE email=?;",
-      [email],
-      (err, rows, fields) => {
-        if (err) throw err;
+  /**
+   * https://stackoverflow.com/questions/60476055/javascript-promises-unhandledpromiserejectionwarning
+   * try/catch also works but let is out of scope
+   */
+  let [rows, fields] = await connection
+    .query("SELECT * FROM users WHERE email=?;", [email])
+    .catch((err) => {
+      // Do not throw error inside of promise
+      console.log(err);
+    });
 
-        // falsey, if 0 then there is no user with the same email
-        if (rows.length) {
-          // Error out if email exists
-          res.status(400).send({ msg: "Email already exists!" });
-        } else {
-          // Use hash function from utils/bcrypt.js
-          const password_hash = hash(req.body.password_hash);
-          /**
-           * .execute(), prepared statement parameters are sent from the client as a serialized string and handled by the server
-           * The VALUES(?) is standard way to insert variables into a SQL statement using an array
-           */
-          connection.execute(
-            "INSERT INTO users (first_name, last_name, email, type, password_hash) VALUES (?, ?, ?, ?, ?);",
-            [first_name, last_name, email, type, password_hash],
-            (err, rows, fields) => {
-              // Error checking for bad query
-              if (err) throw err;
-              console.log("Values inserted!");
+  // Release connection after destructuring, cannot use .then
+  await connection.release();
 
-              // Release connection
-              connection.release();
-            }
-          );
-        }
-
-        //New session creator if student type given.
-        if (type == "student") {
-          //Pulls ID of current user
-          connection.query(
-            "SELECT id FROM users WHERE email=?;",
-            [email],
-            (err, user_id, fields) => {
-              if (err) throw err;
-
-              //Creates new session with ID
-              connection.execute(
-                "INSERT INTO session (user_id, intake_date, school_administrator, social_worker, school_counselor, student_pickup) VALUES (?, ?, ?, ?, ?, ?);",
-                [
-                  user_id[0].id,
-                  intake_date,
-                  school_admin,
-                  social_worker,
-                  school_counselor,
-                  pickup,
-                ],
-                (err, rows, fields) => {
-                  // Error checking for bad query
-                  if (err) throw err;
-
-                  console.log("More success!");
-
-                  // Release connection
-                  connection.release();
-                }
-              );
-            }
-          );
-        }
-
-        // Release connection
+  // falsey, if 0 then there is no user with the same email
+  if (rows.length) {
+    // Error out if email exists
+    res.status(400).send({ msg: "Email already exists!" });
+  } else {
+    // Use hash function from utils/bcrypt.js
+    const password_hash = hash(req.body.password_hash);
+    /**
+     * .execute(), prepared statement parameters are sent from the client as a serialized string and handled by the server
+     * The VALUES(?) is standard way to insert variables into a SQL statement using an array
+     */
+    await connection
+      .execute(
+        "INSERT INTO users (first_name, last_name, email, type, password_hash) VALUES (?, ?, ?, ?, ?);",
+        [first_name, last_name, email, type, password_hash]
+      )
+      .then(() => {
+        console.log("Values inserted!");
         connection.release();
-      }
-    );
-    // Successful HTTPS
-    res.sendStatus(201);
-  });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  //New session creator if student type given.
+  if (type == "student") {
+    //Pulls ID of current user
+    let [user_id, fields] = await connection
+      .query("SELECT id FROM users WHERE email=?;", [email])
+      .catch((err) => {
+        console.log(err);
+      });
+
+    await connection.release();
+
+    //Creates new session with ID
+    await connection
+      .execute(
+        "INSERT INTO session (user_id, intake_date, school_administrator, social_worker, school_counselor, student_pickup) VALUES (?, ?, ?, ?, ?, ?);",
+        [
+          user_id[0].id,
+          intake_date,
+          school_admin,
+          social_worker,
+          school_counselor,
+          pickup,
+        ]
+      )
+      // .then() can be used because promise not destructured
+      .then(() => {
+        console.log("More success!");
+        connection.release();
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+  // Successful HTTPS
+  res.sendStatus(201);
 });
 
 module.exports = router;
