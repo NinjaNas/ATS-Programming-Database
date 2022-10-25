4d7311cffdcf9d535ab6a7add6771b7826e10c42
Khang Tran <ktra@email.unc.edu>
2022-10-25 19:01:23 -0400

    Use aync/await and getConnection

--- server/routes/users/index.js
+++ server/routes/users/index.js
@@ -2,38 +2,41 @@
 const express = require("express");
 const router = express.Router();
 const create = require("./create");
-const pool = require("../../utils/pool");
+const { getConnection } = require("../../utils/pool");
+const { authorize } = require("../../utils/authorize");
+
+router.use(authorize());
 
 // Routing
 router.use("/create", create);
 
-pool.getConnection(function (err, connection) {
-  // Bad connection
-  if (err) throw err;
+/**
+ * GET request handler for returning users table
+ *
+ * '/' - route path will match requests to the root route (in this case it would be '/users')
+ * req - Receives GET request
+ * res - Send back HTTPS result
+ */
+router.get("/", async (req, res) => {
+  // Await connection
+  let connection = await getConnection();
   /**
-   * GET request handler for returning users table
-   *
-   * '/' - route path will match requests to the root route (in this case it would be '/users')
-   * req - Receives GET request
-   * res - Send back HTTPS result
+   * .query(), parameter substitution is handled on the client, including objects
+   * 'SELECT * FROM users' is valid sql to select everything from the table 'users'
+   *  rows is an array containing each row as an object
+   *  fields is an array containing each field as an object
    */
-  router.get("/", (req, res) => {
-    /**
-     * .query(), parameter substitution is handled on the client, including objects
-     * 'SELECT * FROM users' is valid sql to select everything from the table 'users'
-     *  rows is an array containing each row as an object
-     *  fields is an array containing each field as an object
-     */
-    connection.query("SELECT * FROM users", (err, rows, fields) => {
-      // Error checking for bad query
-      if (err) throw err;
-
+  await connection
+    .query("SELECT * FROM users;")
+    .then((rows, fields) => {
       // Send HTTPS
       res.send(rows);
-
       // Release connection
       connection.release();
+    })
+    .catch((err) => {
+      console.log(err);
     });
-  });
 });
+
 module.exports = router;
