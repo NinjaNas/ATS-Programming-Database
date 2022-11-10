65f39ef30bf88940b43c379bc966bb8f2cfe3218
Khang Tran <ktra@email.unc.edu>
2022-11-10 17:21:53 -0500

    Jest index.js

--- server/routes/user/index.js
+++ server/routes/user/index.js
@@ -4,8 +4,8 @@ const router = express.Router();
 const create = require("./create");
 const update = require("./update");
 const del = require("./delete");
-const pool = require("../../utils/pool");
 const { authorize } = require("../../utils/authorize");
+const { indexController } = require("../../controllers/user/index");
 
 // Routing
 router.use("/create", create);
@@ -19,22 +19,6 @@ router.use("/delete", del);
  * req - Receives GET request
  * res - Send back HTTPS result
  */
-router.get("/", authorize(["admin", "counselor"]), async (req, res) => {
-  /**
-   * .query(), parameter substitution is handled on the client, including objects
-   * 'SELECT * FROM users' is valid sql to select everything from the table 'user'
-   *  rows is an array containing each row as an object
-   *  fields is an array containing each field as an object
-   */
-  await pool
-    .query("SELECT * FROM users;")
-    .then((table) => {
-      // Send HTTPS, promises return the table access rows at 0 and fields at 1
-      res.send(table[0]);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
+router.get("/", authorize(["admin", "counselor"]), indexController);
 
 module.exports = router;
