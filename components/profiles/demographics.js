80c5250339d8b92ab136fb270cafc1661c9d8eb8
Elad Ohana <elad.ohana@gmail.com>
2022-11-13 12:38:19 -0500

    Created input, dropdown and checkbox fields

--- components/profiles/demographics.js
+++ components/profiles/demographics.js
@@ -1,5 +1,8 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import Axios from "axios";
+import InputForm from '../form/input';
+import Dropdown from '../form/dropdown';
+import CheckBox from '../form/checkbox';
 
 function Demographics({id}) {
   const [demographics, setDemographics] = useState([]);
@@ -8,6 +11,9 @@ function Demographics({id}) {
       setDemographics(response.data.filter(s => s.user_id == id));
     });
   };
+
+  const genderRef = useRef()
+  const raceRef = useRef()
 
   const gender = {
     1: "Female", 
@@ -44,9 +50,15 @@ function Demographics({id}) {
     <div>
       {demographics.map(data => (
           <>
-            <p>Date of Birth: {(new Date(data.date_of_birth)).toDateString()}</p>
-            <p>Gender: {gender[data.gender]}</p>
-            <p>Race: {displayRace(data)}</p>
+            <p>Date of Birth: {(new Date(data.date_of_birth)).toLocaleDateString()}</p>
+            <label>Date of Birth</label>
+            <input type="date" defaultValue={(new Date(data.date_of_birth)).toLocaleDateString('en-CA')} />
+            {/* <p>Gender: {gender[data.gender]}</p> */}
+            {/* <InputForm label="Gender" ref={genderRef} passedValue={gender[data.gender]} /> */}
+            <Dropdown label="Gender" ref={genderRef} passedValue={data.gender} passedOptions={gender}/>
+
+            {/* <p>Race: {displayRace(data)}</p> */}
+            <CheckBox label="Race" ref={raceRef} passedValue = {data} passedOptions={race} />
           </>
           )
         )
