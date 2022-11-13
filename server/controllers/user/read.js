6936f962fd28077e7f2ff9329ad2ccf596e953be
ZackBarbari <zackb@unc.edu>
2022-11-13 11:22:48 -0500

    Routes to user added

--- server/controllers/user/read.js
+++ server/controllers/user/read.js
@@ -2,7 +2,7 @@ const pool = require("../../utils/pool");
 
 async function readController(req, res) {
   // Get user ID from req body
-  const { key, tag, tag2 } = req.body;
+  const { key, tag } = req.body;
 
   let [rows, fields] = await pool
     .query("SELECT type FROM users WHERE id=?;", [user_id])
@@ -12,20 +12,8 @@ async function readController(req, res) {
     });
 
   if (rows.length) {
-    //0 key reads names, 1 key reads UUID, 2 key outputs all, 3 key outputs active
+    //0 key reads UUID, 1 key outputs all, 2 key outputs active
     if (key == 0) {
-        let [rows, fields] = await pool
-        .query("SELECT * FROM users WHERE first_name=?;", [tag])
-        .then((table) => {
-            // Send HTTPS, promises return the table access rows at 0 and fields at 1
-            res.send(table[0]);
-          })
-        .catch((err) => {
-            // Do not throw error inside of promise
-            console.log(err);
-        });
-    }
-    else if (key == 1) {
         let [rows, fields] = await pool
         .query("SELECT * FROM users WHERE id=?;", [tag])
         .then((table) => {
@@ -36,7 +24,7 @@ async function readController(req, res) {
             // Do not throw error inside of promise
             console.log(err);
         });
-    } else if (key == 2) {
+    } else if (key == 1) {
         let [rows, fields] = await pool
         .query("SELECT * FROM users;")
         .then((table) => {
