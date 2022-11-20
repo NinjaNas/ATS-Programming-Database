31bbafa3e3c149c9392658691360a9027c840a9f
Khang Tran <ktra@email.unc.edu>
2022-11-20 01:08:23 -0500

    Added functionality for read task

--- server/controllers/session/task/read.js
+++ server/controllers/session/task/read.js
@@ -5,32 +5,46 @@ async function readController(req, res) {
   const { key, tag } = req.query;
   // console.log(key, tag)
 
-  //0 key reads session_id, 1 key reads all completed but not verified, 2 key reads all 
+  //0 key reads session_id, 1 key reads all completed but not verified, 2 key reads task_id, -1 key reads all
   if (key == 0) {
-      await pool.query("SELECT * FROM task WHERE session_id=?;", [tag])
+    await pool
+      .query("SELECT * FROM task WHERE session_id=?;", [tag])
       .then((table) => {
-          // Send HTTPS, promises return the table access rows at 0 and fields at 1
-          res.send(table[0]);
-        })
+        // Send HTTPS, promises return the table access rows at 0 and fields at 1
+        res.send(table[0]);
+      })
       .catch((err) => {
-          // Do not throw error inside of promise
-          console.log(err);
+        // Do not throw error inside of promise
+        console.log(err);
       });
   } else if (key == 1) {
-      await pool.query("SELECT * FROM task WHERE status=?;", ["2"])
+    await pool
+      .query("SELECT * FROM task WHERE status=?;", ["2"])
       .then((table) => {
-          res.send(table[0]);
-        })
+        res.send(table[0]);
+      })
       .catch((err) => {
-          console.log(err);
+        console.log(err);
+      });
+  } else if (key == 2) {
+    await pool
+      .query("SELECT * FROM task WHERE id=?;", [tag])
+      .then((table) => {
+        // Send HTTPS, promises return the table access rows at 0 and fields at 1
+        res.send(table[0]);
+      })
+      .catch((err) => {
+        // Do not throw error inside of promise
+        console.log(err);
       });
   } else {
-      await pool.query("SELECT * FROM task;")
+    await pool
+      .query("SELECT * FROM task;")
       .then((table) => {
-          res.send(table[0]);
-        })
+        res.send(table[0]);
+      })
       .catch((err) => {
-          console.log(err);
+        console.log(err);
       });
   }
   // Successful HTTPS
