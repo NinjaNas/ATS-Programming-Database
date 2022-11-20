d528946e3767969395ee8a794833ba9ec8264135
ZackBarbari <zackb@unc.edu>
2022-11-20 15:57:42 -0500

    ID population

--- pages/app/user/profile/[profileid]/index.js
+++ pages/app/user/profile/[profileid]/index.js
@@ -2,35 +2,41 @@ import React from "react";
 import pageStyles from "../../../../../styles/Dashboard.module.css";
 import Navbar from "../../../../../components/dashboard/adminNav.js";
 import Footer from "../../../../../components/dashboard/footer.js";
+import StudentHeader from "../../../../../components/studentProfile/StudentHeader";
 import { useRouter } from "next/router";
 import Axios from "axios";
 import { useState, useEffect } from "react";
 
 function UserProfile() {
     const router = useRouter();
-    const { uid } = router.query;
+    const { userid } = router.query;
     const [user, setUser] = useState();
+    console.log(userid)
 
     const userInfo = () => {
         Axios.get("http://localhost:3000/api/user/read", {
-          params: { key: -1 },
+          params: { key: 0, tag: userid },
         }).then((response) => {
-          // console.log(studentid);
-          //  setStudent(response.data.filter(s => s.id == studentid));
           setUser(response.data[0]);
-          //  setStudent[student.filter(s => student.id == studentid)]
         });
       };
 
       useEffect(() => {
         userInfo();
-      }, [uid]);
+      }, [userid]);
 
 	return (
 		<div className={pageStyles.mainPage}>
 			<Navbar></Navbar>
-            "BOOK"
-			<div style={{ height: "90%" }}></div>
+            {userid && user && (
+                <>
+                    <StudentHeader
+                    key={user.id}
+                    firstName={user.first_name}
+                    lastName={user.last_name}
+                    />
+                </>
+            )}
 			<Footer></Footer>
 		</div>
 	);
