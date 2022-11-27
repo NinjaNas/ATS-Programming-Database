4729119b1a29ea3e0e883a7d7bfe8bd8b4c8e95f
Elad Ohana <elad.ohana@gmail.com>
2022-11-27 12:08:40 -0500

    Added SEL Questionnaire on Days creation

--- pages/app/dashboard/admin/studentprofile/[studentid]/session/[sessionid]/days/add.js
+++ pages/app/dashboard/admin/studentprofile/[studentid]/session/[sessionid]/days/add.js
@@ -48,10 +48,53 @@ const addSession = () => {
         end_date: null
       }
       console.log(body)
-      Axios.post("/api/session/task/create", body).then(response => {
-        router.push(`/app/dashboard/admin/studentprofile/${studentid}`)
-      });
-    }    
+      Axios.post("/api/session/task/create", body).then(response => {console.log(response)}); 
+      // .then(response => {
+      //   router.push(`/app/dashboard/admin/studentprofile/${studentid}`)
+      // });
+    }
+    
+    const q1 = {
+      session_id: sessionid,
+      questionnaire_date: new Date(days[0].attendance_day).toLocaleDateString("en-CA"),
+      // questionnaire_date: days[0],
+      type: 1,
+      question_strengths: 0,
+      question_help: 0,
+      question_pride: 0,
+      question_relationships:0 ,
+      question_collaboration:0 ,
+      question_composure: 0,
+      question_goals: 0,
+      status: 0,
+      notes: null,
+    }
+
+    
+    const q2 = {
+      session_id: sessionid,
+      // questionnaire_date: days[days.length - 1],
+      questionnaire_date: new Date(days[days.length - 1].attendance_day).toLocaleDateString("en-CA"),
+      type: 1,
+      question_strengths: 0,
+      question_help: 0,
+      question_pride: 0,
+      question_relationships:0 ,
+      question_collaboration:0 ,
+      question_composure: 0,
+      question_goals: 0,
+      status: 0,
+      notes: null,
+    }
+
+    console.log(q1);
+    console.log(q2);
+    Axios.post("/api/session/questionnaire/create", q1).then(response => {console.log(response)}); 
+
+    Axios.post("/api/session/questionnaire/create", q2).then(response => {
+      console.log(response);
+      router.push(`/app/dashboard/admin/studentprofile/${studentid}`)
+    });
   }
 
   useEffect(() => {
