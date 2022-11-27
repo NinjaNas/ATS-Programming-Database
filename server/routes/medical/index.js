279828047f82b80ddd37bf8e88c0aa4c12736599
ZackBarbari <zackb@unc.edu>
2022-11-27 12:33:19 -0500

    Medical backend endpoints created

---
+++ server/routes/medical/index.js
@@ -0,0 +1,25 @@
+// Dependencies
+const express = require("express");
+const router = express.Router();
+const update = require("./update");
+const read = require("./read");
+const create = require("./create");
+const { authorize } = require("../../utils/authorize");
+const { indexController } = require("../../controllers/medical");
+
+// Routing
+router.use("/update", update);
+router.use("/read", read);
+router.use("/create", create);
+
+/**
+ * GET request handler for returning session table
+ *
+ * '/' - route path will match requests to the root route (in this case it would be '/medical
+ * ')
+ * req - Receives GET request
+ * res - Send back HTTPS result
+ */
+router.get("/", authorize(["admin", "counselor"]), indexController);
+
+module.exports = router;
