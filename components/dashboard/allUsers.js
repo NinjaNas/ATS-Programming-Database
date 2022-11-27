11847d62e3f321c95179e63c81e198ffac0a27f7
Khang Tran <ktra@email.unc.edu>
2022-11-26 23:09:06 -0500

    Readded create user button

--- components/dashboard/allUsers.js
+++ components/dashboard/allUsers.js
@@ -2,12 +2,16 @@ import React from "react";
 import Axios from "axios";
 import { useState, useEffect } from "react";
 import DashboardStyles from "../../styles/Dashboard.module.css";
+import SearchStyles from "../../styles/Search.module.css";
 import Search from "../dashboard/searchBar.js";
+import { useRouter } from "next/router";
 
 function allUsers() {
   /*Creat state to load user data*/
   const [users, setUsers] = useState([]);
   const [searchedU, setSearchedU] = useState([]);
+
+  const router = useRouter();
   /*Axios call to get user data*/
 
   const everyUser = () => {
@@ -22,10 +26,21 @@ function allUsers() {
     everyUser();
   }, []);
 
+  const createUser = () => {
+    router.push("/app/dashboard/admin/createuser");
+  };
+
   return (
     <div className={DashboardStyles.currentStud}>
       <h3 className={DashboardStyles.title}>All Users</h3>
-      <Search users={users} setUsers={setSearchedU}></Search>
+      <div>
+        <div className={SearchStyles.child}>
+          <Search students={users} setStudents={setSearchedU}></Search>{" "}
+        </div>
+        <div className={SearchStyles.child}>
+          <input type="button" value="Create user" onClick={createUser}></input>{" "}
+        </div>
+      </div>
       <table className={DashboardStyles.subtitle}>
         <tbody>
           <tr>
