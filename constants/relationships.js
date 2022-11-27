97f0b2adad31c1e76dc1a0a960f5e279abb955fe
ZackBarbari <zackb@unc.edu>
2022-11-27 14:05:43 -0500

    Medical update working

---
+++ constants/relationships.js
@@ -0,0 +1,9 @@
+const relationships = {
+    "Parent": "Parent",
+    "Sibling": "Sibling",
+    "Partner": "Partner",
+    "Friend": "Friend",
+    "Other": "Other"
+  }
+  
+  export default relationships
--- pages/app/dashboard/admin/studentprofile/[studentid]/medical.js
+++ pages/app/dashboard/admin/studentprofile/[studentid]/medical.js
@@ -2,10 +2,10 @@ import Axios from "axios";
 import { useRouter } from "next/router"
 import { useEffect, useState } from "react";
 import Navbar from "../../../../../../components/dashboard/adminNav";
-import DemographicsEdit from "../../../../../../components/profiles/demographicsEdit";
+import MedicalEdit from "../../../../../../components/profiles/medicalEdit";
 import StudentHeader from "../../../../../../components/studentProfile/StudentHeader";
 
-const demographics = () => {
+const medical = () => {
   const router = useRouter();
   const { studentid } = router.query
   const [student, setStudent] = useState();
@@ -28,9 +28,9 @@ const demographics = () => {
     <>
       <Navbar />
       {studentid && student && <StudentHeader id={studentid} firstName={student.first_name} lastName={student.last_name}/>}
-      {studentid && <DemographicsEdit id={studentid} />}
+      {studentid && <MedicalEdit id={studentid} />}
     </>
     )
 }
 
-export default demographics
+export default medical
