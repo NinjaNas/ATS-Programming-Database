3e46f4b9bd2ee67e4f885ba988876294f0e121eb
Gabriel Mercado Mendez <gabomm99@ad.unc.edu>
2022-11-28 12:35:04 -0500

    Medical Info is now collapsable

--- components/profiles/medicalRead.js
+++ components/profiles/medicalRead.js
@@ -1,60 +1,64 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useEffect, useRef, useState } from "react";
 import Axios from "axios";
-import { useRouter } from 'next/router';
-import Link from 'next/link';
-import CardStyles from "../../styles/Cards.module.css"
-import race from '../../constants/race';
-import gender from '../../constants/gender';
+import { useRouter } from "next/router";
+import Link from "next/link";
+import CardStyles from "../../styles/Cards.module.css";
+import race from "../../constants/race";
+import gender from "../../constants/gender";
 
-function MedicalRead({id}) {
-  const [medical, setMedical] = useState();
+function MedicalRead({ id }) {
+	const [medical, setMedical] = useState();
+	const [display, setDisplay] = useState("none");
+	const router = useRouter();
 
-  const router = useRouter();
+	const medicalInfo = () => {
+		Axios.get("/api/medical/read/", { params: { key: 0, tag: id } }).then(
+			(response) => {
+				setMedical(response.data[0]);
+			}
+		);
+	};
 
-  const medicalInfo = () => {
-    Axios.get("/api/medical/read/", {params: {key:0, tag:id}}).then((response) => {
-      setMedical(response.data[0]);
-    });
-  };
+	const expand = () => {
+		display == "none" ? setDisplay("block") : setDisplay("none");
+	};
 
+	useEffect(() => {
+		medicalInfo();
+	}, []);
 
+	useEffect(() => {}, [medical]);
 
+	return (
+		<div className={CardStyles.card}>
+      <button className={CardStyles.displayLink} onClick={expand}>Medical</button>
+			{medical && (
+				<div style={{display: display}} >
+					<p>Contact Name: {medical.contact_name}</p>
+					<p>Contact Relationship: {medical.contact_relationship}</p>
+					<p>Contact Phone Number: {medical.contact_phone}</p>
+					<p>Physician: {medical.physician}</p>
 
-  useEffect(() => {    
-    medicalInfo();
-  }, [])
+					<p>
+						Hospital:
+						{(medical.hospital == 1 && " UNC") ||
+							(medical.hospital == 2 && " Duke") ||
+							" Other"}{" "}
+					</p>
+					<p>Medical Concerns: {medical.medical_concerns}</p>
 
-  useEffect(() => {
-  }, [medical])
+					<p>
+						Allergies:
+						{medical.allergies == medical.allergies_list}
+					</p>
 
-
-  return (
-    
-    <div>
-      {medical && (
-          <div className={CardStyles.card}>
-            <h2>Medical</h2>
-            <p>Contact Name: {medical.contact_name}</p>
-            <p>Contact Relationship: {medical.contact_relationship}</p>
-            <p>Contact Phone Number: {medical.contact_phone}</p>
-            <p>Physician: {medical.physician}</p>
-
-            <p>Hospital:
-            {medical.hospital == 1 && " UNC" || medical.hospital == 2 && " Duke" || " Other"} </p>
-            <p>Medical Concerns: {medical.medical_concerns}</p>
-
-            <p>Allergies: 
-            {medical.allergies == medical.allergies_list }</p>
-
-            
-            <Link href={`/app/dashboard/admin/studentprofile/${id}/medical`}><a>Edit Profile</a></Link>
-          </div>
-          )
-        
-      }
-    
-    </div>
-  )
+					<Link href={`/app/dashboard/admin/studentprofile/${id}/medical`}>
+						<a>Edit Profile</a>
+					</Link>
+				</div>
+			)}
+		</div>
+	);
 }
 
-export default MedicalRead
+export default MedicalRead;
