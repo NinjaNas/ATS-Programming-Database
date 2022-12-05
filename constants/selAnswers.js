0bee81b4466bdb2c15dbb737bbda4c2b6ceba03c
Elad Ohana <elad.ohana@gmail.com>
2022-12-04 23:17:36 -0500

    Added selAnswers[5] text for questionnaire

--- constants/selAnswers.js
+++ constants/selAnswers.js
@@ -1,5 +1,5 @@
 const selAnswers = {
-  1: "No", 	2: "A little", 	3: "Somewhat", 	4: "Quite a bit", 
+  1: "No", 	2: "A little", 	3: "Somewhat", 	4: "Quite a bit", 5: "Yes!"
 }
 
 export default selAnswers;
