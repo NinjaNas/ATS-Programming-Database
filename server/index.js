8f3a2faf948dbd21aa055ed95477528fcc52927b
Elad Ohana <elad.ohana@gmail.com>
2022-12-04 22:13:04 -0500

    Added code for adding a default admin if none are present, need to add DEFAULT_ADMIN and DEFAULT_PASSWORD variables to .env

--- server/index.js
+++ server/index.js
@@ -8,6 +8,7 @@ const session = require("express-session");
 const MySQLStore = require("express-mysql-session")(session);
 const pool = require("./utils/pool");
 const routes = require("./routes");
+const { hash } = require("./utils/bcrypt");
 require("./utils/local");
 
 // Use the environment variable PORT or 3000
@@ -34,6 +35,79 @@ const sessionStore = new MySQLStore(
   pool
 );
 
+/**
+ * Checks to see if there are active admin accounts and creates
+ * a default account if no admins are present.
+ */
+async function checkForAdmin() {
+  await pool
+    .query("SELECT * FROM user WHERE type=? AND status<=?", ["admin", 2])
+    .then((table) => {
+      // Successful query
+      // If table is empty, add new account
+      if (table[0].length === 0) {
+        console.log("No Admins Present, attempting to create...");
+        createAdmin();
+      } else {
+        // Otherwise, no action necessary
+        console.log("Admins present, use existing accounts to login.");
+      }
+    }).catch(err => {
+      // Failed query
+      console.log(err);
+    });
+}
+
+/**
+ * Creates a default admin account based on the .env file
+ */
+async function createAdmin() {
+  // check that .env has appropriate variables
+  if (!(process.env.DEFAULT_ADMIN && process.env.DEFAULT_PASSWORD)) {
+    console.log(
+      "No values present in .env file for DEFAULT_ADMIN and DEFAULT_PASSWORD."
+    );
+    return;
+  }
+
+  // create values for default user account
+  const body = {
+    email: process.env.DEFAULT_ADMIN,
+    first_name: "Admin",
+    last_name: "Admin",
+    password_hash: hash(process.env.DEFAULT_PASSWORD),
+    created_at: new Date().toLocaleDateString("en-CA"),
+    pronouns: 3,
+    type: "admin",
+  };
+
+  // Insert values into database
+  await pool
+    .execute(
+      "INSERT INTO user (first_name, last_name, email, pronouns, created_at, type, password_hash) VALUES (?, ?, ?, ?, ?, ?, ?);",
+      [
+        body.first_name,
+        body.last_name,
+        body.email,
+        body.pronouns,
+        body.created_at,
+        body.type,
+        body.password_hash,
+      ]
+    )
+    .then((response) => {
+      // Success
+      console.log("Admin Created!");
+      console.log(
+        `Username: ${process.env.DEFAULT_ADMIN}\nPassword: ${process.env.DEFAULT_PASSWORD}`
+      );
+    })
+    .catch((err) => {
+      // Failure
+      console.log(err);
+    });
+}
+
 app
   // Prepare to go into next.js
   .prepare()
@@ -41,6 +115,7 @@ app
   .then(() => {
     // Create express server
     const server = express();
+    checkForAdmin();
 
     // Cross-origin resource sharing, in order for your server to be accessible by other origins (domains)
     // Enables the express server to respond to preflight requests
